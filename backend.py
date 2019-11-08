b68a98169d47fedf4dd11b04a4e85632ccf367fe
Anton Hvornum <anton.feeds+github@gmail.com>
2019-11-08 01:57:32 +0100

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,94 @@
+import spiderWeb
+from os import urandom
+from hashlib import sha512
+from random import randint
+import json
+
+clients = {}
+access_tokens = {}
+
+def gen_id(entropy_length=256):
+	return sha512(urandom(entropy_length)).hexdigest()
+
+class parser():
+	def parse(self, client, data, headers, fileno, addr, *args, **kwargs):
+		if 'payload' in data and 'access_token' in data:
+			if not data['access_token'] in access_tokens:
+				yield {'status' : 'failed', 'reason' : 'Invalid or expired access token.'}
+				return
+
+			sender_device_id = access_tokens[data['access_token']]
+			partner_info = clients[sender_device_id]['connected_to']
+
+			payload = {**data}
+			del(payload['access_token'])
+			sockets[partner_info['fileno']]['socket'].send(payload)
+
+			yield {'status' : 'successful', 'transmission' : 'sent'}
+
+		elif 'connect' in data and 'access_token' in data:
+			if not data['access_token'] in access_tokens:
+				yield {'status' : 'failed', 'reason' : 'Invalid or expired access token.'}
+				return
+			if not data['connect'] in clients:
+				yield {'status' : 'failed', 'reason' : 'Partner number not registered.'}
+				return
+			if data['connect'] == access_tokens[data['access_token']]:
+				yield {'status' : 'failed', 'reason' : 'Can not connect to yourself, smartass! :)'}
+				return
+			if clients[access_tokens[data['access_token']]]['connected_to']:
+				yield {'status' : 'failed', 'reason' : 'Already connected to a partner.'}
+				return
+
+			sender = access_tokens[data['access_token']]
+			device_id = data['connect']
+			partner_fileno = clients[device_id]['fileno']
+			clients[sender]['connected_to'] = {'fileno' : partner_fileno, 'device_id' : device_id, 'state' : 'pending'}
+			clients[device_id]['connected_to'] = {'fileno' : fileno, 'device_id' : sender, 'state' : 'pending'}
+
+			sockets[partner_fileno]['socket'].send({
+				'connection_from' : sender,
+				'status' : clients[sender]['connected_to']['state']
+			})
+
+			yield {'status' : 'successful', 'connection' : clients[sender]['connected_to']['state'], 'partner' : data['connect']}
+		elif 'accept' in data and 'access_token' in data:
+			if not data['access_token'] in access_tokens:
+				yield {'status' : 'failed', 'reason' : 'Invalid or expired access token.'}
+				return
+
+			sender = access_tokens[data['access_token']]
+			if not clients[sender]['connected_to']['state'] == 'pending':
+				yield {'status' : 'failed', 'reason' : 'Connection is not in a pending state.'}
+				return
+			if not clients[sender]['connected_to']['device_id'] == data['accept']:
+				yield {'status' : 'failed', 'reason' : 'Invalid or expired partner ID.'}
+				return
+
+			device_id = data['accept']
+			partner_fileno = clients[device_id]['fileno']
+
+			clients[sender]['connected_to']['state'] = 'accepted'
+			clients[device_id]['connected_to']['state'] = 'accepted'
+
+			sockets[partner_fileno]['socket'].send({
+				'connected_to' : sender,
+				'status' : clients[device_id]['connected_to']['state'],
+				'publicKey' : clients[sender]['publicKey']
+			})
+
+			yield {'status' : 'successful', 'connection' : clients[device_id]['connected_to']['state'], 'partner' : data['accept'], 'publicKey' : clients[device_id]['publicKey']}
+
+		elif 'register' in data and data['register'] == 'publicKey':
+			
+			device_id = randint(1024, 9999)
+			access_token = gen_id()
+			while device_id in clients:
+				device_id = randint(1024, 9999)
+
+			access_tokens[access_token] = device_id
+			clients[device_id] = {'publicKey' : data['keydata'], 'fileno' : fileno, 'connected_to' : None}
+
+			yield {'status' : 'successful', 'device_id' : device_id, 'access_token' : access_token}
+
+server = spiderWeb.server({'default' : parser()}, address='', port=6789)
